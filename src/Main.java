1714d93a7604f832afedf566bf68c872e455799b
alinahyder <alina@granitewood.com>
2024-06-03 23:38:41 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double creditCardBal= 5000;
+        double interestRate= 0.17;
+        double oneMonth;
+        double twoMonth;
+
+        oneMonth= creditCardBal*interestRate;
+        twoMonth= oneMonth*interestRate;
+        System.out.println("The interest due after one month is "+oneMonth);
+        System.out.println("The interest due after two months is "+twoMonth);
+
+    }
+}
